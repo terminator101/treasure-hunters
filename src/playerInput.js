f3f9cb36c90195ca5be382f996a80db824e3fbde
Andrej <andrej101h@gmail.com>
2020-11-25 15:05:02 -0500

    Styling fixes
    - Fixed some text colors
    - FIxed the remove player button being too small

--- src/playerInput.js
+++ src/playerInput.js
@@ -5,9 +5,9 @@ const PlayerInput = ({ idx, playerState, handlePlayerChange, handlePlayerRemove 
     const playerId = `name-${idx}`;
     const computerId = `computer-${idx}`;
     return (
-        <div key={`player-${idx}`} className={"border " + playerState[idx].playerClass + "color mb-2 text-white"}>
+        <div key={`player-${idx}`} className={"border " + playerState[idx].playerClass + "color mb-2"}>
             <div className="form-row">
-                <div className="form-group col-3 mb-0">
+                <div className="form-group col-3 text-white mb-0">
                     <label htmlFor={ playerId }>&nbsp; {`Player #${idx + 1}`}</label>&nbsp; 
                 </div>
                 <div className="form-group col-7 mb-0">
@@ -26,7 +26,7 @@ const PlayerInput = ({ idx, playerState, handlePlayerChange, handlePlayerRemove 
                 </div>
                 {//Display the remove button but only if there are at least two players and only display this for the last player
                 (idx > 0 && idx === (playerState.length - 1)) ?
-                <div className="form-group col-1 mb-0">
+                <div className="form-group col-2 text-white mb-0">
                     <button type="button" onClick={() => handlePlayerRemove(idx) } className="btn btn-danger">X</button>
                 </div>
                 : ""}
@@ -34,7 +34,7 @@ const PlayerInput = ({ idx, playerState, handlePlayerChange, handlePlayerRemove 
             {/* Implement once the computer player is ready  
             idx > 0 ?
             <div className="form-row">
-                <div className="form-check col-10 offset-3">
+                <div className="form-check col-10 offset-3 text-white">
                     <input
                         name="computer"
                         id={ computerId }
