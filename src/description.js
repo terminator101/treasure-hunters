5f65182e96a8e2960f17c42e12c561a2e49e581a
Andrej <andrej101h@gmail.com>
2022-04-11 00:23:42 -0400

    Updated description.js to use hooks

--- src/description.js
+++ src/description.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CARD_TYPES, 
 	CARD_IMAGE_LOCATION } from './constants';
 import Button from 'react-bootstrap/Button';
@@ -7,102 +7,16 @@ import Collapse from 'react-bootstrap/Collapse';
 /**
  * Game description object
  */
-export default class Description extends React.Component {
+const Description = (props) => {
 
-	constructor(props) {
-		super(props);
+	const treasuresForWin = props.treasuresForWin;
+	const unitsPerPlayer = props.unitsPerPlayer;
 
-		this.defaults = {
-			treasuresForWin: this.props.treasuresForWin,
-			unitsPerPlayer: this.props.unitsPerPlayer,
-            cardInformation: this._generateCardInformation(CARD_TYPES),
-		}
-
-		this.state = {
-			open: false,
-		}
-	}
-	
-	_openInfo(){
-		this.setState({ 
-			open: !this.state.open 
-		});
-	}
-
-	render(){
-        this.settings = Object.assign({}, this.defaults, this.props);
-        const cardInformation = this._displayCardInformation(this.settings.cardInformation);
-		return(
-            <div>
-				<Collapse in={ this.state.open }>
-        			<div id="collapse-info">
-						<div className="row justify-content-center">
-							<div className="col-lg-5">
-								<table className="table table-bordered">
-									<thead className="thead-dark">
-										<tr>
-											<th scope="col">Card</th>
-											<th scope="col">Description</th>
-										</tr>
-									</thead>
-									<tbody>
-										{ cardInformation }
-									</tbody>
-								</table>
-							</div>
-						</div>
-						<div className="row justify-content-center">
-							<div className="col-lg-8">
-								<ul>
-									<li>
-										The <span className="font-weight-bold">goal</span> of the game is to use your divers to find treasure(s) and bring it to your boat!
-									</li>
-									<li>
-										Each player starts with <span className="font-weight-bold">{ this.settings.unitsPerPlayer }</span> divers and losing both will end the game
-									</li>
-									<li>
-										Start by selecting your boat and then select an adjacent tile to move
-									</li>
-									<li>
-										A boat can move to the highlighted <span className="outer-water font-weight-bold">outer water</span> tiles, and moved regardless if there are any divers on it. It cannot move to the <span className="shallows font-weight-bold">shallows</span>
-									</li>
-									<li>
-										A diver (circle) can move to the <span className="shallows font-weight-bold">shallows</span>, as well as to and from a boat, and can only carry one treasure at a time
-									</li>
-									<li>
-										Once a shark is killed, it acts same as a tile with fish and if there is a treasure on it, it can be retrieved
-									</li>
-									<li>
-										Delivering <span className="font-weight-bold">{ this.settings.treasuresForWin }</span> treasure(s) to your boat will win the game
-									</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</Collapse>
-				<div className="row justify-content-center">
-					<div className="col-lg-8">
-						<div className="row justify-content-end">
-							<div className="col-6 col-md-3">
-								<Button className="btn btn-primary" type="button"
-									onClick={ () => this._openInfo() }
-									aria-controls="collapse-info"
-									aria-expanded={ this.state.open }>
-										{ this.state.open ? "Hide" : "Show" } instructions
-								</Button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div> 
-		);
-    }
-    
-    /**
+	/**
 	 * Generate the card information
 	 * @param {array} cardTypesArray
 	 */
-	_generateCardInformation(cardTypesArray){
+	const _generateCardInformation = (cardTypesArray) => {
 		let generatedInfo = [];
 
 		cardTypesArray.forEach(function(card, key) {
@@ -113,14 +27,19 @@ export default class Description extends React.Component {
 			});
 		})
 		return generatedInfo;
-	}
+	};
+
+	const [cardInformation] = useState({
+		..._generateCardInformation(CARD_TYPES)
+	});
+	const [open, setOpen] = useState( false );
 
 	/**
 	 * Display The card information
 	 * @param {array} cardArray 
 	 */
-	_displayCardInformation(cardArray){
-		return cardArray.map((cardInfo) => {
+	const _displayCardInformation = (cardArray) => {
+		return Object.values(cardArray).map((cardInfo) => {
 			return(
 				<CardInfo
 					key = {cardInfo.cardType}
@@ -130,7 +49,74 @@ export default class Description extends React.Component {
 			)
 		});
 	}
+
+	return (
+		<div>
+			<Collapse in={ open }>
+				<div id="collapse-info">
+					<div className="row justify-content-center">
+						<div className="col-lg-5">
+							<table className="table table-bordered">
+								<thead className="thead-dark">
+									<tr>
+										<th scope="col">Card</th>
+										<th scope="col">Description</th>
+									</tr>
+								</thead>
+								<tbody>
+									{ _displayCardInformation(cardInformation) }
+								</tbody>
+							</table>
+						</div>
+					</div>
+					<div className="row justify-content-center">
+						<div className="col-lg-8">
+							<ul>
+								<li>
+									The <span className="font-weight-bold">goal</span> of the game is to use your divers to find treasure(s) and bring it to your boat!
+								</li>
+								<li>
+									Each player starts with <span className="font-weight-bold">{ unitsPerPlayer }</span> divers and losing both will end the game
+								</li>
+								<li>
+									Start by selecting your boat and then select an adjacent tile to move
+								</li>
+								<li>
+									A boat can move to the highlighted <span className="outer-water font-weight-bold">outer water</span> tiles, and moved regardless if there are any divers on it. It cannot move to the <span className="shallows font-weight-bold">shallows</span>
+								</li>
+								<li>
+									A diver (circle) can move to the <span className="shallows font-weight-bold">shallows</span>, as well as to and from a boat, and can only carry one treasure at a time
+								</li>
+								<li>
+									Once a shark is killed, it acts same as a tile with fish and if there is a treasure on it, it can be retrieved
+								</li>
+								<li>
+									Delivering <span className="font-weight-bold">{ treasuresForWin }</span> treasure(s) to your boat will win the game
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</Collapse>
+			<div className="row justify-content-center">
+				<div className="col-lg-8">
+					<div className="row justify-content-end">
+						<div className="col-6 col-md-3">
+							<Button className="btn btn-primary" type="button"
+								onClick={ () => setOpen( !open ) }
+								aria-controls="collapse-info"
+								aria-expanded={ open }>
+									{ open ? "Hide" : "Show" } instructions
+							</Button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> 
+	);
 };
+
+export default Description;
 
 /**
  * Card Information object
